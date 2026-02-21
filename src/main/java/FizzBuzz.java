91dc1cf33916c15f33bfee8af12e3c99dd0f148f
Laura Daniela Rodríguez Sánchez <160368603+LauraRo166@users.noreply.github.com>
2026-02-21 17:18:08 +0000

    tests multiplo 3 y 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
     public static String fizzbuzz(int n) {
-        return String.valueOf(n);
+        if ((n % 5 == 0) & (n % 3 == 0)){
+            return "FizzBuzz";
+        } else if (n % 5 == 0){
+            return "Buzz";
+        } else if (n % 3 == 0){
+            return "Fizz";
+        }else{
+            return String.valueOf(n);
+        }
     }
 }
